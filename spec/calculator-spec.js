b3afa10ec534a6e1c6918bc95f727252e233159b
Student <example@example.com>
2019-06-28 14:29:09 -0700

    Add spec finding current date

--- spec/calculator-spec.js
+++ spec/calculator-spec.js
@@ -2,7 +2,12 @@ import { today } from '../src/sample-class.js'
 
 describe ("date", function () {
 
-  it('should return the current date', function() {
+  it('should return the current day', function() {
     expect(today()).toEqual(28);
   });
+
+  it('should return full date', function() {
+    expect(currentDate()).toBe("June 28, 2019");
+  });
+
 });
